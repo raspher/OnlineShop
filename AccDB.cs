229282e5b467c89552509e2769ea6b8e80508049
raspher <szymonscholz@gmail.com>
2021-01-19 17:04:44 +0100

    c

--- AccDB.cs
+++ AccDB.cs
@@ -59,11 +59,49 @@ namespace OnlineShop
             WczytajZamowienia();
         }
 
+        public void WczytajWszystkieUzytkownika(Konto konto)
+        {
+            WczytajAdresyU(konto);
+            WczytajKontoU(konto);
+            WczytajOcenyU(konto);
+            WczytajProdukty();
+            WczytajTransakcjeU(konto);
+            WczytajZamowieniaU(konto);
+        }
+
         public void WczytajAdresy()
         {
             conn.Open();
 
             var zapytanie = "Select * from Adresy";
+            OleDbCommand komenda = new OleDbCommand(zapytanie, conn);
+            OleDbDataReader reader = komenda.ExecuteReader();
+
+            adresy.Clear();
+            while (reader.Read())
+            {
+                Adres adres = new Adres(
+                    reader.GetInt32(0),     // id
+                    reader.GetString(1),    // miejscowosc
+                    reader.GetString(2),    // ulica
+                    reader.GetString(3),    // dom
+                    reader.GetString(4),    // kod pocztowy
+                    reader.GetString(5),    // imie
+                    reader.GetString(6),    // nazwisko
+                    reader.GetInt32(7),     // telefon
+                    reader.GetString(8)     // użytkownik
+                    );
+                adresy.Add(adres);
+            }
+
+            conn.Close();
+        }
+
+        public void WczytajAdresyU(Konto konto)
+        {
+            conn.Open();
+
+            var zapytanie = $"Select * from Adresy WHERE Użytkownik='{konto.nazwa}'";
             OleDbCommand komenda = new OleDbCommand(zapytanie, conn);
             OleDbDataReader reader = komenda.ExecuteReader();
 
@@ -110,11 +148,58 @@ namespace OnlineShop
             conn.Close();
         }
 
+        public void WczytajKontoU(Konto kontou)
+        {
+            conn.Open();
+
+            var zapytanie = $"Select * from Konta WHERE Użytkownik='{kontou.nazwa}'";
+            OleDbCommand komenda = new OleDbCommand(zapytanie, conn);
+            OleDbDataReader reader = komenda.ExecuteReader();
+
+            konta.Clear();
+            while (reader.Read())
+            {
+                Konto konto = new Konto(
+                    reader.GetString(0),    // uzytkownik
+                    reader.GetString(1),    // email
+                    reader.GetString(2),    // haslo
+                    reader.GetString(3)     // rola
+                    );
+                konta.Add(konto);
+            }
+
+            conn.Close();
+        }
+
         public void WczytajOceny()
         {
             conn.Open();
 
             var zapytanie = "Select * from Oceny";
+            OleDbCommand komenda = new OleDbCommand(zapytanie, conn);
+            OleDbDataReader reader = komenda.ExecuteReader();
+
+            oceny.Clear();
+            while (reader.Read())
+            {
+                Ocena ocena = new Ocena(
+                    reader.GetInt32(0),     // id
+                    reader.GetInt32(1),     // ocena
+                    reader.GetString(2),    // komentarz
+                    reader.GetString(3),    // uzytkownik
+                    reader.GetString(4)     // produkt
+                    );
+                oceny.Add(ocena);
+            }
+
+            conn.Close();
+        }
+
+        public void WczytajOcenyU(Konto konto)
+        {
+            conn.Open();
+
+            var zapytanie = $"Select * from Oceny Użytkownik='{konto.nazwa}'";
             OleDbCommand komenda = new OleDbCommand(zapytanie, conn);
             OleDbDataReader reader = komenda.ExecuteReader();
 
@@ -215,11 +300,56 @@ namespace OnlineShop
             conn.Close();
         }
 
+        public void WczytajTransakcjeU(Konto konto)
+        {
+            conn.Open();
+
+            var zapytanie = $"Select * from Transakcje WHERE Użytkownik='{konto.nazwa}'";
+            OleDbCommand komenda = new OleDbCommand(zapytanie, conn);
+            OleDbDataReader reader = komenda.ExecuteReader();
+
+            transakcje.Clear();
+            while (reader.Read())
+            {
+                Transakcja transakcja = new Transakcja(
+                    reader.GetString(0),     // paragon
+                    reader.GetString(1),    // uzytkownik  
+                    reader.GetFloat(2)    // kwota
+                    );
+                transakcje.Add(transakcja);
+            }
+
+            conn.Close();
+        }
+
         public void WczytajZamowienia()
         {
             conn.Open();
 
             var zapytanie = "Select * from Zamówienia";
+            OleDbCommand komenda = new OleDbCommand(zapytanie, conn);
+            OleDbDataReader reader = komenda.ExecuteReader();
+
+            zamowienia.Clear();
+            while (reader.Read())
+            {
+                Zamowienie zamowienie = new Zamowienie(
+                    reader.GetInt32(0),     // id
+                    reader.GetString(1),    // paragon
+                    reader.GetString(2),    // produkt
+                    reader.GetInt32(3)      // adres
+                    );
+                zamowienia.Add(zamowienie);
+            }
+
+            conn.Close();
+        }
+
+        public void WczytajZamowieniaU(Konto konto)
+        {
+            conn.Open();
+
+            var zapytanie = $"Select * from Zamówienia WHERE Użytkownik='{konto.nazwa}'";
             OleDbCommand komenda = new OleDbCommand(zapytanie, conn);
             OleDbDataReader reader = komenda.ExecuteReader();
 
